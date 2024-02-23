05603da9f868a563db7ca823d64c95356c0021c7
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-24 02:24:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+
+form.addEventListener('submit' , async (e) => {
+	e.preventDefault();;
+	const age = form.age.value;
+	const name = form.name.value;
+	promisecall(age).then(() =>{
+		alert(`Welcome, ${name}. You can vote.`);
+	}).catch(() => {
+		alert(`Oh sorry ${name}. You aren't old enough.`);
+	});
+
+	form.reset();
+});
+
+function promisecall(age){ return new Promise((res , rej) => {
+	if(age > 18){
+		setTimeout(((age) => {
+			res();
+		}) , 4000);
+	}else{
+		rej();
+	}
+});
+					 }
